3b73a8462851e749237a6f4e533cacc2bcb94a79
Sun <do.you.love.me777@gmail.com>
2012-07-18 16:16:39 +0300

    2 done?
    trahtibedoh tibedoh

--- src/lv/ctco/java/coop/Puzzle.java
+++ src/lv/ctco/java/coop/Puzzle.java
@@ -57,7 +57,11 @@ public class Puzzle {
 		int result = 0;
         for(int i = 0; i < 100; i++)
         {
-
+            if(i % 3 == 0)
+                System.out.println("Fizz");
+            else if(i % 5 == 0)
+                System.out.println("Buzz");
+            else System.out.println(i);
         }
 		/*
 		 * Your code goes here
